7ff7a26ebac15dab78586f09478f10fd726f9c08
Mako <61922615+Makonede@users.noreply.github.com>
2025-07-27 21:06:24 -0700

    fix: make `settings` a `SvelteMap`
    Signed-off-by: Mako <61922615+Makonede@users.noreply.github.com>

--- frontend/src/lib/settings.svelte.ts
+++ frontend/src/lib/settings.svelte.ts
@@ -15,6 +15,8 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { SvelteMap } from 'svelte/reactivity'
+
 export let settings = $state({
-  colors: new Map<string, string>(),
+  colors: new SvelteMap<string, string>(),
 })
