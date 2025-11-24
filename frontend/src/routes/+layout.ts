220a4897f7f6dc1c97a62378f75ad9754cf552ac
Mako <61922615+Makonede@users.noreply.github.com>
2025-11-23 17:08:02 -0800

    fix: remove unused import
    Signed-off-by: Mako <61922615+Makonede@users.noreply.github.com>

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -16,8 +16,6 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-import type { Load } from '@sveltejs/kit'
-
 import { loadTranslations } from '$lib/translations.svelte'
 
 export const prerender = true
